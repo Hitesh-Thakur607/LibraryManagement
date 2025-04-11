a0d926f0240e5774395973bac0a768b9e29c7ab8
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-11 15:13:16 +0530

    Update cookie.js

--- utils/cookie.js
+++ utils/cookie.js
@@ -9,7 +9,7 @@ const sendcookie = (user, res, message, statusCode) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "None",
         maxAge: 3600000, 
     });
 
