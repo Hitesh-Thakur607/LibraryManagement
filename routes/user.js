c38e8ae356b6b79455960b389ade1af2b273da88
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-11 16:55:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,5 @@ const express=require("express");
 const router=express.Router();
 router.post("/register",register);
 router.post("/login",login);
-router.post("/me",getuserdetails);
+router.get("/me",getuserdetails);
 module.exports=router;
