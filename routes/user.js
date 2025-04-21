a90701906843e79d96aacd3e4c12f0486dda3b71
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-21 12:29:54 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
-const{register,login,getuserdetails} =require("../controller/user.js");
+const{register,login,getuserdetails,logout} =require("../controller/user.js");
 const {isauthenticated}=require("../middleware/auth.js");
 const express=require("express");
 const router=express.Router();
 router.post("/register",register);
 router.post("/login",login);
 router.get("/me",isauthenticated,getuserdetails);
+router.get("/logout",isauthenticated,logout);
 module.exports=router;
