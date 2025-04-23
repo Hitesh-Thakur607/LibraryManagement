05cabcf6cb96c3ce1295b662325d90b50506b215
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-23 14:00:26 +0530

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -1,31 +1,3 @@
-// const express=require("express");
-// const router = express.Router();
-
-// const {isauthenticated}=require("../middleware/auth.js");
-// const {    getAllBooks,
-//     getBookById,
-//     postBook,
-//     borrowBook,
-//     returnBook,
-//     deleteBook} = require("../controller/book.js");
-
-// router.get("/allbooks",isauthenticated,getAllBooks);
-// router.get("/:id",isauthenticated,getBookById);
-// router.post("/postbook",isauthenticated,postBook);
-// router.put("/borrowBook/:id",isauthenticated,borrowBook);
-// router.put("/returnBook/:id",isauthenticated,returnBook);
-// router.delete("/deletebook/:id",isauthenticated,deleteBook);
-
-// module.exports = router;returnBook
-// // {
-// //     "name":"hello",
-// //     "description":"jai bhole ki"
-// // }
-// // {
-// //     "name":"kaduwala",
-// //    "email":"kadulelo@gmai.com",
-// //     "password":"123456"
-// // }
 const express=require("express");
 const router = express.Router();
 const{logout} =require("../controller/user.js");
