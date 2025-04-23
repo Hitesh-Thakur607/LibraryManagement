67367c348b849bf859a33cf45cbcb4941a92ffdb
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-23 12:57:52 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,15 +1,83 @@
+// const express=require("express");
+// const router = express.Router();
+// const generateId = () => Math.floor(Math.random() * 1000000);
+// const {sendcookie}=require("../utils/cookie");
+// const {users}=require("../Model/user.js");
+// const register=async(req,res)=>{
+//     const { name, email, password } = req.body;
+//     let user = await users.findOne({ email });
+
+//     if (user) return res.status(400).json("User already exists");
+
+//     // const hashedPassword = await bcrypt.hash(password, 10);
+//     user = await users.create({ name, email, password});
+//     return res.status(200).json({message:"User registered successfully"});
+// }
+// const login = async (req, res) => {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//         return res.status(400).json({ message: "All fields are required" });
+//     }
+
+//     try {
+//         // Find the user with matching email and password
+//         const user = await users.findOne({ email, password }); // Mongoose method
+
+//         if (user) {
+//             sendcookie(user, res, `Login successful, ${user.name}`, 201);
+//         } else {
+//             res.status(400).json({ message: "Invalid credentials" });
+//         }
+//     } catch (error) {
+//         console.error("Login Error:", error);
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
+//  const getuserdetails = (req, res, next) => {
+//   try {
+//     if (!req.user) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Unauthorized: No user found",
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       user: req.user,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+// const logout=async(req,res)=>{
+//     res.cookie("token",null,{
+//         expires:new Date(Date.now()),
+//         httpOnly:true,
+//         secure:true,
+//         sameSite:"none"
+//     });
+//     return res.status(200).json({message:"Logged out successfully"});
+// }
+// module.exports={register,login,getuserdetails,logout};
+// // {
+// //     "name":"kaduwala",
+// //    "email":"kadulelo@gmai.com",
+// //     "password":"123456"
+// // }
 const express=require("express");
 const router = express.Router();
 const generateId = () => Math.floor(Math.random() * 1000000);
 const {sendcookie}=require("../utils/cookie");
 const {users}=require("../Model/user.js");
+const {admin}=require("../Model/admin.js");
 const register=async(req,res)=>{
     const { name, email, password } = req.body;
     let user = await users.findOne({ email });
 
     if (user) return res.status(400).json("User already exists");
 
-    // const hashedPassword = await bcrypt.hash(password, 10);
     user = await users.create({ name, email, password});
     return res.status(200).json({message:"User registered successfully"});
 }
@@ -21,8 +89,8 @@ const login = async (req, res) => {
     }
 
     try {
-        // Find the user with matching email and password
-        const user = await users.findOne({ email, password }); // Mongoose method
+
+        const user = await users.findOne({ email, password }); 
 
         if (user) {
             sendcookie(user, res, `Login successful, ${user.name}`, 201);
@@ -34,23 +102,23 @@ const login = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
- const getuserdetails = (req, res, next) => {
-  try {
-    if (!req.user) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: No user found",
-      });
+const adminlogin=async(req,res)=>{
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
     }
-
-    res.status(200).json({
-      success: true,
-      user: req.user,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+    try {
+        const adminx = await admin.findOne({ email, password });
+        if (adminx) {
+            sendcookie(adminx, res, `Login successful, ${adminx.name}`, 201);
+        } else {
+            res.status(400).json({ message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.error("Login Error:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
 const logout=async(req,res)=>{
     res.cookie("token",null,{
         expires:new Date(Date.now()),
@@ -60,7 +128,7 @@ const logout=async(req,res)=>{
     });
     return res.status(200).json({message:"Logged out successfully"});
 }
-module.exports={register,login,getuserdetails,logout};
+module.exports={register,login,logout,adminlogin};
 // {
 //     "name":"kaduwala",
 //    "email":"kadulelo@gmai.com",
