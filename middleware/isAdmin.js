da644647975e9ec1976507f626b98188e15b4b90
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-23 12:58:28 +0530

    Add files via upload

---
+++ middleware/isAdmin.js
@@ -0,0 +1,13 @@
+// middleware/isAdmin.js
+const jwt = require("jsonwebtoken");
+// const {admin}=require("../Model/admin.js");
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.role === "admin") {
+        next();
+    } else {
+        res.status(403).json({ message: "Access denied. Admin only." });
+    }
+};
+
+module.exports = { isAdmin };
+
