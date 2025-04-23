4b53ed7e9f534212a2a28cca97f0269ad5b5da87
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-04-23 12:59:54 +0530

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -1,21 +1,53 @@
+// const express=require("express");
+// const router = express.Router();
+
+// const {isauthenticated}=require("../middleware/auth.js");
+// const {    getAllBooks,
+//     getBookById,
+//     postBook,
+//     borrowBook,
+//     returnBook,
+//     deleteBook} = require("../controller/book.js");
+
+// router.get("/allbooks",isauthenticated,getAllBooks);
+// router.get("/:id",isauthenticated,getBookById);
+// router.post("/postbook",isauthenticated,postBook);
+// router.put("/borrowBook/:id",isauthenticated,borrowBook);
+// router.put("/returnBook/:id",isauthenticated,returnBook);
+// router.delete("/deletebook/:id",isauthenticated,deleteBook);
+
+// module.exports = router;returnBook
+// // {
+// //     "name":"hello",
+// //     "description":"jai bhole ki"
+// // }
+// // {
+// //     "name":"kaduwala",
+// //    "email":"kadulelo@gmai.com",
+// //     "password":"123456"
+// // }
 const express=require("express");
 const router = express.Router();
-
+const{logout} =require("../controller/user.js");
 const {isauthenticated}=require("../middleware/auth.js");
+const {isAdmin}=require("../middleware/isAdmin.js");
 const {    getAllBooks,
     getBookById,
     postBook,
     borrowBook,
     returnBook,
     deleteBook} = require("../controller/book.js");
-
+    router.get("/logout",isauthenticated,logout);
 router.get("/allbooks",isauthenticated,getAllBooks);
 router.get("/:id",isauthenticated,getBookById);
-router.post("/postbook",isauthenticated,postBook);
+// router.post("/postbook",isauthenticated,postBook);
 router.put("/borrowBook/:id",isauthenticated,borrowBook);
 router.put("/returnBook/:id",isauthenticated,returnBook);
-router.delete("/deletebook/:id",isauthenticated,deleteBook);
+// router.delete("/deletebook/:id",isauthenticated,deleteBook);
 
+router.post("/postbook", isauthenticated, isAdmin, postBook);
+
+router.delete("/books/:id", isauthenticated, isAdmin, deleteBook);
 module.exports = router;returnBook
 // {
 //     "name":"hello",
