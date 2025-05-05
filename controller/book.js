a7b8358d6d621fe1fb6fb0819b6401a1df58ffbd
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-05-05 13:28:38 +0530

    Update book.js

--- controller/book.js
+++ controller/book.js
@@ -3,7 +3,8 @@ const { books } = require("../Model/books.js");
 
 const getAllBooks = async (req, res) => {
   try {
-    const allBooks = await books.find(); // Fetch all books from the DB
+    // const allBooks = await books.find(); // Fetch all books from the DB
+    const allbooks = await books.find().populate("borrowedby", "name email");
     if (allBooks.length === 0) {
       return res.status(200).json({ message: "No books available" });
     }
