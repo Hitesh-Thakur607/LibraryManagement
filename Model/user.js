f083de70c339f35090e08cb301a896d1d7b0b2db
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-05-13 22:44:04 +0530

    Update user.js

--- Model/user.js
+++ Model/user.js
@@ -1,3 +1,27 @@
+// const mongoose =require( "mongoose");
+// const schema = new mongoose.Schema({
+//   name: String,
+//   email: {
+//     type:String,
+//     required:true,
+//     unique:true
+//   },
+//   password: {type:String,
+//     required:true,
+//       select:false,
+//   },
+//   createdate:{
+//     type:Date,
+//     required:true,
+//     default:Date.now
+//   },
+//   role: {
+//     type: String,
+//     default: "user"
+//   }
+// });
+//  const users=mongoose.model("users",schema);
+//  module.exports={users};
 const mongoose =require( "mongoose");
 const schema = new mongoose.Schema({
   name: String,
@@ -18,7 +42,11 @@ const schema = new mongoose.Schema({
   role: {
     type: String,
     default: "user"
-  }
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
 });
  const users=mongoose.model("users",schema);
  module.exports={users};
